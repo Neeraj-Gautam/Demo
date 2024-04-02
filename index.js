9c0c8bd3f989e5e8686d852e735b81bb01b54672
Neeraj <neeraj23gautam@gmail.com>
2024-04-02 10:20:26 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/health", (req, res) => {
+    res.json({
+      service: "Demo server",
+      status: "Active",
+      time: new Date(),
+    });
+  });
+  
+
+  const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
+
+module.exports = app;
